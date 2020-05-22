4f56101fd5b833d9b861f2f729acab7607c5b39b
Gary Smailes <garysmailes@gmail.com>
2020-05-22 10:04:29 +0100

    Add file safety

--- app/controllers/editorial_services_controller.rb
+++ app/controllers/editorial_services_controller.rb
@@ -13,12 +13,19 @@ class EditorialServicesController < ApplicationController
     def pricing
     end
 
+    def file_safety
+      @page_title = "File Safety"
+      @page_description = "Discover how we ensure your manuscript is kept safe." 
+    end
+
     private
 
     def resolve_layout
       case action_name
       when "book_editing"
         "template_no_container"
+      when "file_safety"
+        "template_narrow"
       else
         "application"
       end
