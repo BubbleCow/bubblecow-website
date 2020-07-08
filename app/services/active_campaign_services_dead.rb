b057b2308a6a73932136aec97814a0b5fc59136b
Gary Smailes <garysmailes@gmail.com>
2020-07-08 16:23:21 +0100

    Remove AC servcie rb file

--- app/services/active_campaign_services_dead.rb
+++ app/services/active_campaign_services_dead.rb
@@ -1,19 +1,19 @@
 class ActiveCampaignService
   
-    def contact_tag_add(email, tags)
-        return unless is_production?
-        client.contact_tag_add(email: email, tags: tags)
-    end
+  #   def contact_tag_add(email, tags)
+  #       return unless is_production?
+  #       client.contact_tag_add(email: email, tags: tags)
+  #   end
   
-    private
+  #   private
   
-    def client
-      @client ||= ActiveCampaign.new(
-        api_endpoint: Rails.application.credentials.active_campaign[:AC_CAMPAIGN_URL],
-        api_key: Rails.application.credentials.active_campaign[:AC_CAMPAIGN_KEY])
-    end
+  #   def client
+  #     @client ||= ActiveCampaign.new(
+  #       api_endpoint: Rails.application.credentials.active_campaign[:AC_CAMPAIGN_URL],
+  #       api_key: Rails.application.credentials.active_campaign[:AC_CAMPAIGN_KEY])
+  #   end
   
-    def is_production?
-      Rails.env.production?
-    end
-  end
+  #   def is_production?
+  #     Rails.env.production?
+  #   end
+  # end
