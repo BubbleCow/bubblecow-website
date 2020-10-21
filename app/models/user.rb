74ca7eecbd8a9693280c0ffa8697c79db4a25b7f
Gary Smailes <garysmailes@gmail.com>
2020-10-21 18:42:35 +0100

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
   has_person_name
+  
+  enum role: [:writer, :editor, :admin]
+
+  after_initialize do
+    if self.new_record?
+      self.role ||= :writer
+    end
+  end
 
   has_many :notifications, foreign_key: :recipient_id
   has_many :services
