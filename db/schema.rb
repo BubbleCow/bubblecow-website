dcbe63d91998718355912f11bb9b195d0ff19187
garysmailes <garysmailes@gmail.com>
2021-07-14 10:21:46 +0100

    Update active storage

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_202746) do
+ActiveRecord::Schema.define(version: 2021_07_14_090334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,14 @@ ActiveRecord::Schema.define(version: 2020_11_19_202746) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "announcements", force: :cascade do |t|
@@ -100,6 +107,22 @@ ActiveRecord::Schema.define(version: 2020_11_19_202746) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "mentorship_sessions", force: :cascade do |t|
+    t.integer "mentorship_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mentorships", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_mentorships_on_slug", unique: true
+  end
+
   create_table "messages", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -108,9 +131,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_202746) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "sender_name"
-    t.datetime "discarded_at"
     t.boolean "archived", default: false
-    t.index ["discarded_at"], name: "index_messages_on_discarded_at"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -205,15 +226,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_202746) do
     t.index ["slug"], name: "index_testimonials_on_slug", unique: true
   end
 
-  create_table "useredits", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "developmental_edit_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["developmental_edit_id"], name: "index_useredits_on_developmental_edit_id"
-    t.index ["user_id"], name: "index_useredits_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -237,7 +249,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_202746) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "services", "users"
-  add_foreign_key "useredits", "developmental_edits"
-  add_foreign_key "useredits", "users"
 end
