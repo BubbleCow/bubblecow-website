afe3ac672c7ec9edf355e465a7027d95a93cd804
garysmailes <garysmailes@gmail.com>
2022-01-28 15:23:20 +0000

    Tep policy change for user

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,11 +1,13 @@
 class UserPolicy < ApplicationPolicy
 
 	def index?
-		is_managerial_staff?
+		# is_managerial_staff?
+		true
 	end
 
 	def show?
-		is_managerial_staff?
+		# is_managerial_staff?
+		true?
 	end
 
 	def edit?
@@ -13,11 +15,13 @@ class UserPolicy < ApplicationPolicy
 	end
 
 	def update?
-		is_managerial_staff?
+		# is_managerial_staff?
+		true
 	end
 
 	def destroy?
-		is_managerial_staff?
+		# is_managerial_staff?
+		true
 	end
 
 end
