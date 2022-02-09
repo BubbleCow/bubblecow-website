08192fb89fef686d25dfab8702fa63b86871c488
garysmailes <garysmailes@gmail.com>
2022-02-09 14:29:30 +0000

    Add mentoring

--- app/controllers/customer_pages_controller.rb
+++ app/controllers/customer_pages_controller.rb
@@ -1,11 +1,13 @@
 class CustomerPagesController < ApplicationController
   before_action :authenticate_user!, only: [:blog_dashboard]
+  before_action :set_mentoring_price, only: [:mentoring] 
   layout :set_template
 
   def index
     @page_title = "Affordable Book Editing Services"
     @page_description = "Book editing services for serious writers. Serving writers since 2007. 100+ testimonials."
     @testimonial_count = Testimonial.all.count
+    @messages = Message.all
   end
 
   def about
@@ -28,16 +30,49 @@ class CustomerPagesController < ApplicationController
     @page_description = "Discover how we ensure your manuscript is kept safe." 
   end
 
+  def mentoring
+    @page_title = "Mentoring"
+    @page_description = "One-to-one professional mentoring for writers." 
+    @message = Message.new
+  end
+  
+
   private
 
   def set_template
     case action_name
-    when 'index'
+    when 'index', 'mentoring'
       'full_width'
     when 'writing_manual'
       'backend'
     else
       'application'
+    end
+  end
+
+
+  private
+
+  def set_mentoring_price
+
+    if Rails.env.development?
+      @country = "GB"
+    else
+      @country = request.location.country
+      # @country = "US" # - if country fails active this and block term above
+    end
+
+    case @country
+
+    when "GB", "United Kingdom"
+      @currency_symbol = "&#163;".html_safe
+      @mentoring_cost = "325"
+      @mentoring_price = @currency_symbol + @mentoring_cost
+    else 
+      @user_country = "United States"
+      @currency_symbol = "&#36;".html_safe
+      @mentoring_cost = "500"
+      @mentoring_price = @currency_symbol + @mentoring_cost
     end
   end
 
