6d6940a4f3b110e26fa8777a15ac0ea62547e401
garysmailes <garysmailes@gmail.com>
2022-04-15 08:28:47 +0100

    Add GA

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,11 @@ import * as bootstrap from "bootstrap"
 import "trix"
 import "@rails/actiontext"
 import "chartkick/chart.js"
+
+
+document.addEventListener("turbo:load", function(event) {
+    window.dataLayer = window.dataLayer || []
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date())
+    gtag('config', 'G-CN4JXE1772', {'page_location': event.detail.url})
+  }, false)
