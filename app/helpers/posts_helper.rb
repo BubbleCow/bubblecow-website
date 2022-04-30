81a1df3441499f6eb12d823801d6492f06c2fac4
garysmailes <garysmailes@gmail.com>
2022-04-30 10:41:01 +0100

    Add content to post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+
+    def post_content
+        # if content is present, it shows that, otherwise it defaults to active_text/trix content -->
+        if @post.content.present? 
+            sanitize @post.content 
+        else
+            @post.body
+        end
+    end
+
 end
--- app/javascript/trix_extensions.js
+++ app/javascript/trix_extensions.js
@@ -66,16 +66,5 @@ function toolbarDefaultHTML() {
        <button type="button" class="trix-button trix-button--icon trix-button--icon-redo" data-trix-action="redo" data-trix-key="shift+z" title="${lang.redo}" tabindex="-1">${lang.redo}</button>
      </span>
    </div>
-   <div class="trix-dialogs" data-trix-dialogs>
-     <div class="trix-dialog trix-dialog--link" data-trix-dialog="href" data-trix-dialog-attribute="href">
-       <div class="trix-dialog__link-fields">
-         <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="${lang.urlPlaceholder}" aria-label="${lang.url}" required data-trix-input>
-         <div class="trix-button-group">
-           <input type="button" class="trix-button trix-button--dialog" value="${lang.link}" data-trix-method="setAttribute">
-           <input type="button" class="trix-button trix-button--dialog" value="${lang.unlink}" data-trix-method="removeAttribute">
-         </div>
-       </div>
-     </div>
-   </div>
 `;
 }
