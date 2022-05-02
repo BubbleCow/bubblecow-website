32a52e0844db7f4b0363085166bf3b331a21d9f1
garysmailes <garysmailes@gmail.com>
2022-05-02 10:18:01 +0100

    Update css and htmlblog posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     def post_content
         # if content is present, it shows that, otherwise it defaults to active_text/trix content -->
         if @post.content.present? 
-            sanitize @post.content 
+            content_tag(:div, @post.content.html_safe, class: 'blog-post' )
         else
             @post.body
         end
