690db3618eb49237eb05b6e4a704cffe2cb7431e
garysmailes <garysmailes@gmail.com>
2022-05-05 09:51:56 +0100

    Fix

--- app/javascript/google_tag_manager.js
+++ app/javascript/google_tag_manager.js
@@ -1,16 +1,10 @@
+
+
+
 // GTM
 document.addEventListener("turbo:load", function(event) {
-    var url = event.data.url;
-
-    dataLayer.push({
-      'event':'pageView',
-      'virtualUrl': url
-    });
-  });
-
-
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K67GGW9');
+  window.dataLayer = window.dataLayer || []
+  function gtag(){dataLayer.push(arguments)}
+  gtm('js', new Date())
+  gtm('config', 'GTM-K67GGW9', {'page_location': event.detail.url})
+}, false)
