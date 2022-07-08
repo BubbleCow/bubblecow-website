e08060baacaaad59bc0b0b03b12bdaba607eefae
garysmailes <garysmailes@gmail.com>
2022-07-08 16:46:55 +0100

    Add ac tag to course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_122937) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_150302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_122937) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ac_tag"
     t.index ["slug"], name: "index_courses_on_slug", unique: true
   end
 
