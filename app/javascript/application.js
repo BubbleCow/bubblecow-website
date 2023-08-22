9dc12da2b32ec38ea0b58d344808c49d76131df5
Gary Smailes <garysmailes@gmail.com>
2023-08-22 17:25:53 +0100

    Add chartkick and other JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,22 +6,10 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+import "chartkick"
+import "Chart.bundle"
 
-// Popover
-document.addEventListener("DOMContentLoaded", function () {
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-    const popoverList = Array.from(popoverTriggerList).map(function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl);
-    });
-  });
-
-// Tooltips
-document.addEventListener("turbo:load", function () {
-  // This code is copied from Bootstrap's docs. See link below.
-  var tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
-});
+// Custom
+import "custom/bootstrap_extensions";
+import "custom/trix_extensions";
+import "custom/google_tag_manager";
