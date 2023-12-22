3d2b00d6d5bad2a56a257cfe7e68c0d41b249078
Gary Smailes <garysmailes@gmail.com>
2023-12-22 10:37:13 +0000

    Add parent and child category relationship

--- app/helpers/post_categories_helper.rb
+++ app/helpers/post_categories_helper.rb
@@ -1,2 +1,20 @@
 module PostCategoriesHelper
-end
+    # Generates hierarchical category options for dropdowns
+    def category_options(exclude_category = nil)
+      category_list = []
+      build_category_list(PostCategory.where(parent_category_id: nil), category_list, exclude_category)
+      category_list
+    end
+  
+    private
+  
+    # Recursive method to build category list
+    def build_category_list(categories, category_list, exclude_category, prefix = '')
+      categories.each do |category|
+        next if category == exclude_category
+        category_list << [prefix + category.name, category.id]
+        build_category_list(category.child_categories, category_list, exclude_category, prefix + '-- ')
+      end
+    end
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_09_17_081107) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_22_101250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -236,6 +236,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_09_17_081107) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_category_id"
     t.index ["slug"], name: "index_post_categories_on_slug", unique: true
   end
 
@@ -378,6 +379,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_09_17_081107) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "orders", "books"
+  add_foreign_key "post_categories", "post_categories", column: "parent_category_id"
   add_foreign_key "services", "users"
   add_foreign_key "taggings", "tags"
 end
